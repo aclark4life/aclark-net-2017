b302cc3e5b7db806ba5688be6df88944f4b9f593
Alex Clark <aclark@aclark.net>
2017-12-12 18:25:06 -0500

    Update

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var BundleTracker = require("webpack-bundle-tracker");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require("webpack");
+
+module.exports = {
+    context: __dirname + "/project/app/assets",
+    entry: "./entry",
+    output: {
+        path: __dirname + "/project/app/assets/webpack_bundles",
+        filename: "[name]-[hash].js"
+    },
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new ExtractTextPlugin("[name]-[hash].css"),
+        // http://getbootstrap.com/docs/4.0/getting-started/webpack/#importing-javascript
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery',
+          Popper: ['popper.js', 'default'],
+        })
+    ],
+    // https://github.com/webpack-contrib/extract-text-webpack-plugin#usage
+    module: {
+      loaders: [
+        {   
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: "css-loader"
+            })
+        },
+      ],
+    },  
+};
