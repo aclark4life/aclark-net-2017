c10503e0c14d51ef24946eea56908b16746647cc
Alex Clark <aclark@aclark.net>
2017-12-12 18:35:20 -0500

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aclarknet.db',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
